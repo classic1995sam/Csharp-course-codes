bf12f6c2f7a444769377305fde9ee97038bb0f24
classic1995sam <classic.sam@hotmail.com>
2017-06-22 06:41:45 +0300

    first C# proj'

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is my first program in C#
+            Console.WriteLine("HELLO WORLD");
+            Console.ReadKey();
+        }
+    }
+}
